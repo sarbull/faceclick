7d9c0099f9df6d5325da22f8928a481b0d1063b0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-03-05 05:19:37 +0200

    added commands.html

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/commands', function(req, res){
+  res.sendFile(__dirname + '/commands.html');
+});
+
 io.on('connection', function(socket){
   socket.broadcast.emit('new_user', 'a new user has entered!');
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    console.log('message: ' + msg);
   });
 
   socket.on('disconnect', function(){
     io.emit('disconnected', "a user has disconnected");
-    console.log('user disconnected');
+  });
+
+  socket.on('show_red', function(){
+    console.log('show_red');
+    io.emit('show_red');
+  });
+
+  socket.on('hide_red', function(){
+    console.log('hide_red');
+    io.emit('hide_red');
   });
 });
 
